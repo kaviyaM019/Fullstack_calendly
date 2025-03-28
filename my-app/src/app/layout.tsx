2f3fe1eb5fe2fe05bb52feaa2c99d633bd384fec
kaviyaM019 <you@example.com>
2025-03-28 18:31:22 +0530

    bug fixed

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
+import Header from "./components/Header";
 import "./globals.css";
 
 // const geistSans = Geist({
@@ -25,10 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={noto.className}
-      >
+    <body className={noto.className}>
+      <main className="container">
+        <Header />
+
+      
         {children}
+      </main>
       </body>
     </html>
   );
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,24 +1,26 @@
 
-// import { CalendarDays } from "lucide-react";
-// import Link from "next/link";
-import Header from "./components/Header";
+import Hero from "@/app/components/Hero";
 
 
 export default function Home() {
   return (
-    <main className="container">
-      <Header />
-      <section className="text-center mt-24">
-        <h1 className="text-4xl font-bold mb-6">Scheduling made simple</h1>
-        <p>
-          Most Scheduling apps are simple but ours is even more simple.<br/>
-          On top of this, it's open source and you can see the code. 
-        </p>
+    <>
+    <Hero/>
+      <section className="text-center mt-32">
+        <p className="text-gray-600">Trusted by those companies: </p>
+        <div className="flex gap-8 *:h-6 mt-6 justify-center">
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/7hhEfCdZPI7mPC24fFsC94/adf392604ef673a224307e48ac87bd96/Adobe.svg" alt="" />
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/4Y87kRrhSPSYgUbSWYxP1z/a13177cf43f99e7a79c691c54e271a98/Hubspot.svg" alt="" />
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/7dsuPwH4V8KJvCexSZueZc/272b2ef619de8ae4b443758413a19733/Unbounce_Logo.svg"alt="" />
+          <img src="https://images.ctfassets.net/lh3zuq09vnm2/5F3AqUH61Ll0Oa6eRQKyuT/7ea7e484e1e46495ed4fae2a1162a575/TechSmith.svg" alt="" />
 
+
+        </div>
       </section>
-    
-    </main>
-      
-    
+
+
+    </>
+   
+
   );
 }
